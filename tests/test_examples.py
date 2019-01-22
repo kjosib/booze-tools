e1b8258ce9e495bab30d8123600c5f1ac11e0e32
Ian Kjos <kjosib@gmail.com>
2019-01-22 16:12:29 -0600

    Improve test coverage of JSON example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,4 +6,33 @@ class TestJson(unittest.TestCase):
 	
 	def test_00_smoke_test(self):
 		self.assertEqual(25.2, json.parse('25.2'))
+	
+	def test_01_glossary_entry(self):
+		""" See https://json.org/example.html """
+		text = """
+{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+		"""
+		entry = json.parse(text)
+		self.assertEqual('Standard Generalized Markup Language', entry['glossary']['GlossDiv']['GlossList']['GlossEntry']['GlossTerm'])
 		
