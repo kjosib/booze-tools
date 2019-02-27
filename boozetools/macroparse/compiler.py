cf966d9e2d3d2bdcb6be2b40b4922574afcb4bdb
Ian Kjos <kjosib@gmail.com>
2019-02-27 17:04:38 -0600

    Update compiler.py

--- boozetools/macroparse/compiler.py
+++ boozetools/macroparse/compiler.py
@@ -3,13 +3,62 @@ MacroParse will be built atop the existing "mini" parsing and scanning tools.
 It extends the language of attributed context-free grammars with additional features described on the wiki page.
 
 The design for this module is still in flux, although most of the main ideas are laid out.
+The concept is a single-file definition of both lexical and syntactic analysis embedded within MarkDown text.
+
+Most components of such a format are straightforward to analyze with available string operations or
+simple applications of standard Python regex machinery. However, because the "productions" blocks support
+a no-kidding macro language, it's both expedient and illustrative to use miniparse and miniscan definitions
+to analyze that portion of the definitions.
+
+For the moment, some lexical and production rules are here:
+
+========================================
+\s+         :ignore
+\l\w*       :name
+:\l\w*      :action
+[.]/\S      :capture
+[][(),|]    :passthru
+'\S+'       :literal
+"\S+"       :literal
+[-=>:<]+    :arrow
+========================================
+For the purpose of getting the macroparse compiler up and running with minimal fuss, the moral equivalent of
+two macro definitions will be implemented: these are list_of(what, sep) and one_or_more(what).
+These will be helper functions which create a gensym, add the two constituent rules to it, and return
+the gensym. Those rules look like:
+
+list_of(what, sep) -> what :first | .list_of(what, sep) sep .what :append
+one_or_more(what) -> what :first | .one_or_more(what) .what :append
+
+From the structure of the above, it is apparent that elaborating a macro call is a tricky
+business. The algorithm is:
+1. Using an equivalence classifer, determine the gensym appropriate to the call.
+2. If the gensym is freshly allocated this call, then:
+	Make sure it won't be seen as fresh in case of recursion, and then
+	for each formal production:
+		Substitute the bound symbols corresponding to the call into actual productions on the gensym.
+ 
+========================================
+production -> .head arrow .list_of(rewrite, '|')
+head -> :use_prior | name :declare_nonterminal | .name '(' .list_of(name, ',' ) ')' :declare_macro
+alternatives -> rewrite :first | .alternatives '| .rewrite :append
+rewrite -> element :first | rewrite element :append
+element -> symbol | capture symbol
+actual -> name | literal
+| '[' one_or_more(actual) ']' :gensym_renaming
+| .name '(' .actuals ')' :macro_call
+
+symbol -> action |
+formals -> name :first | .formals ', .name :append
+
 """
 import re
 from boozetools import context_free, miniscan, regular, foundation
 
 class DefinitionError(Exception): pass
 
-INITIAL = 'INITIAL'
+
+
 
 def compile_file(pathname):
 	with(open(pathname)) as fh: document = fh.read()
@@ -69,7 +118,7 @@ def compile_string(document:str):
 			return conditions
 		if head == 'patterns':
 			nonlocal group
-			group = tokens[1] if len(tokens)>1 else INITIAL
+			group = tokens[1] if len(tokens)>1 else 'INITIAL'
 			return patterns
 		if head == 'precedence': return precedence
 		if head == 'productions':
