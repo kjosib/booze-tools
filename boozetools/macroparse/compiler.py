f0dc3d2fa6c15018f7d988b61fea484a4083cd39
Ian Kjos <kjosib@gmail.com>
2019-03-06 16:16:15 -0600

    Update compiler.py

--- boozetools/macroparse/compiler.py
+++ boozetools/macroparse/compiler.py
@@ -16,6 +16,7 @@ For the moment, some lexical and production rules are here:
 \s+         :ignore
 \l\w*       :name
 :\l\w*      :action
+%\l+        :pragma
 [.]/\S      :capture
 [][(),|]    :passthru
 '\S+'       :literal
@@ -40,29 +41,81 @@ business. The algorithm is:
  
 ========================================
 production -> .head arrow .list_of(rewrite, '|')
-head -> :use_prior | name :declare_nonterminal | .name '(' .list_of(name, ',' ) ')' :declare_macro
-alternatives -> rewrite :first | .alternatives '| .rewrite :append
-rewrite -> element :first | rewrite element :append
-element -> symbol | capture symbol
+head -> :use_prior | name | .name '(' .list_of(name, ',' ) ')'
+rewrite -> one_or_more(element) :normal_rewrite
+| .one_or_more(element) pragma_precsym .[name literal] :prec_rewrite
+element -> symbol | capture symbol :capturing
+symbol -> actual | action
 actual -> name | literal
-| '[' one_or_more(actual) ']' :gensym_renaming
-| .name '(' .actuals ')' :macro_call
-
-symbol -> action |
-formals -> name :first | .formals ', .name :append
+| '[' .one_or_more(actual) ']' :gensym_renaming
+| .name '(' .list_of(actual, ',') ')' :macro_call
 
 """
 import re
-from boozetools import context_free, miniscan, regular, foundation
+from boozetools import context_free, miniscan, regular, foundation, miniparse, algorithms
 
 class DefinitionError(Exception): pass
-
-
-
 
 def compile_file(pathname):
 	with(open(pathname)) as fh: document = fh.read()
 	return compile_string(document)
+
+PROD_LEX = miniscan.Definition()
+
+### Here's a set of semantic categories for the breakdown of rewrite rules:
+class Element:
+	""" Base class of RHS elements -- more a marker than anything here... """
+
+class Action(Element):
+	def __init__(self, name): self.name = name
+
+class Symbol(Element):
+	def __init__(self, name): self.name = name
+
+class GensymRenaming(Element):
+	def __init__(self, alternatives): self.alternatives = alternatives
+
+class MacroCall(Element):
+	def __init__(self, name, actuals): self.name, self.actuals = name, actuals
+
+class Rewrite:
+	def __init__(self, elements:list, precsym):
+		self.elements = elements
+		self.precsym = precsym
+		self.message = self.elements.pop(-1).name if isinstance(self.elements[-1], Action) else None
+		self.__args = args = []
+		self.size = len(self.elements)
+		for i,elt in enumerate(self.elements):
+			if hasattr(elt, 'capture'): args.append(i)
+		if not args: args.extend(range(self.size)) # Pick up everything if nothing is special.
+	def prefix_capture(self, size:int):
+		return tuple(c - size for c in self.__args if c < size)
+
+
+# The MacroParse metagrammar makes use of macro facilities.
+# Here's one way to bake something like it atop the miniparser:
+def list_of(what, sep):
+	head = 'list_of(%r,%r)'%(what,sep)
+	PRODUCTION.rule(head, '.%s'%what)(FIRST)
+	PRODUCTION.rule(head, '.%s %s .%s'%(head, sep, what) )(APPEND)
+	return head
+def one_or_more(what):
+	head = 'one_or_more(%r)' % (what)
+	PRODUCTION.rule(head, '.%s' % what)(FIRST)
+	PRODUCTION.rule(head, '.%s .%s' % (head, what))(APPEND)
+	return head
+def FIRST(element): return [element]
+def APPEND(the_list, element):
+	the_list.append(element)
+	return the_list
+
+# Now that the meta-macro-facility is supplied, the syntax definition for production rule lines is fairly mechanical.
+PRODUCTION = miniparse.MiniParse('production')
+PRODUCTION.rule('production', '.head arrow .'+list_of('rewrite', '|'))
+PRODUCTION.rule('head', '', lambda :None)
+PRODUCTION.renaming('head', 'name',)
+PRODUCTION.rule('.name ( .%s )'%list_of('name', ','))(tuple) # I'll represent macro heads with (name, args) tuples.
+
 
 def compile_string(document:str):
 	# The approach is a sort of outside-in parse. The outermost layer concerns the overall markdown document format,
@@ -85,7 +138,8 @@ def compile_string(document:str):
 		if not m: raise DefinitionError('Unable to analyze overall pattern/action/parameter/(rank) structure at line %d.'%line_number)
 		pattern, action, parameter, rank_string = m.groups()
 		rank = int(rank_string) if rank_string else 0
-		bol, expression, trail = miniscan.analyze_pattern(pattern, env)
+		try: bol, expression, trail = miniscan.analyze_pattern(pattern, env)
+		except algorithms.LanguageError as e: raise DefinitionError('Malformed pattern on line %d.'%line_number) from e
 		rule_id = foundation.allocate(nfa_actions, (action, parameter, trail, line_number))
 		src = nfa.new_node(rank)
 		dst = nfa.new_node(rank)
@@ -100,8 +154,29 @@ def compile_string(document:str):
 		pass
 	
 	def productions():
-		assert False, 'Code for this block is not designed yet.'
-		pass
+		nonlocal recent_nonterminal
+		try: head, list_of_rewrites = PRODUCTION.parse(PROD_LEX.scan(s))
+		except algorithms.LanguageError: raise DefinitionError()
+		# Deal with nonterminal elision:
+		if head is None:
+			if recent_nonterminal is None: raise DefinitionError('At line %d, there should be a nonterminal introduced before rewrites make sense.' % line_number)
+			else: head = recent_nonterminal
+		else:
+			recent_nonterminal = head
+			if isinstance(head, tuple):
+				name, formals = head
+				if name in macro_definitions:
+					_, _, prior = macro_definitions[name]
+					raise DefinitionError('Macro %r is first declared on line %d, then redeclared on line %d. Place all alternatives together between \'|\' symbols, please.'%(name, prior, line_number))
+				else:
+					macro_definitions[name] = formals, [], line_number
+		if isinstance(head, tuple): macro_definitions[head[0]][1].extend(list_of_rewrites)
+		else:
+			for rewrite in list_of_rewrites:
+				assert isinstance(rewrite, Rewrite)
+				rule_id = alloc_production(rewrite.message, rewrite.prefix_capture(rewrite.size))
+				cfg.rule(head, desugar(rewrite, {}), rule_id, rewrite.precsym)
+				
 	
 	def decide_section():
 		# Looks at a header line to see which parsing mode/section to shift into based on a leading keyword,
@@ -122,16 +197,27 @@ def compile_string(document:str):
 			return patterns
 		if head == 'precedence': return precedence
 		if head == 'productions':
+			nonlocal recent_nonterminal
+			recent_nonterminal = None
 			for t in tokens[1:]:
 				if t not in start:
 					start.append(t)
 			return productions
 		return None
+	# Supporting functions:
+	def alloc_production(message, captures): return foundation.allocate(cfg_actions, (message, captures, line_number))
+	def desugar(rewrite:Rewrite, env:dict) -> list:
+		for i, elt in enumerate(rewrite.elements):
+			pass
+		assert False, 'Code for this block is not designed yet.'
+		pass
 	
 	# The context-free portion of the definition:
+	recent_nonterminal = None
 	cfg = context_free.ContextFreeGrammar()
-	cfg_actions = [] # The format of a cfg action entry shall be the pair <message_name, tuple-of-offsets>
+	cfg_actions = [] # The format of a cfg action entry shall be the tuple <message_name, tuple-of-offsets, line_number>
 	start = []
+	macro_definitions = {}
 	
 	# The regular (finite-state) portion of the definition:
 	env = miniscan.PRELOAD['ASCII'].copy()
@@ -171,6 +257,13 @@ def main():
 	args = parser.parse_args()
 	assert args.source_path.lower().endswith('.md')
 	target_path = args.output or args.source_path[:-3]+'.mdc'
-	compile_file(args.source_path).serialize_to(target_path)
+	try:
+		compile_file(args.source_path).serialize_to(target_path)
+	except DefinitionError as e:
+		import sys
+		print(e.args[0], file=sys.stderr)
+		exit(1)
+	else:
+		pass
 
 if __name__ == '__main__': main()
