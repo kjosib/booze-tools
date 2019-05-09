66e12a42a4d07a988f69b3b3a5157f2a30a2738c
Ian Kjos <kjosib@gmail.com>
2019-05-09 00:29:57 -0500

    Add the GLR(0) construction.

--- boozetools/GLR.py
+++ boozetools/GLR.py
@@ -11,28 +11,73 @@ then taking a further step to resolve any conflicts. That is why I'm going down 
 """
 import typing, collections
 
-from . import context_free
+from . import context_free, foundation
 
-class GLR_State():
+END = '<END>' # An artificial "end-of-text" terminal-symbol.
+
+class GLR0_State(typing.NamedTuple):
+	items: set
+	shift: dict
+	reduce: set
+
+class GLR0_Construction:
 	"""
-	This generic structure for a GLR(k) parse state is convenient while constructing
-	parse tables. Later on, another structure may be better for actually using them.
-	"""
-	def __init__(self, core):
-		self.core = frozenset(core)
-		self.extension = set() # all active parse items; initially empty; facilitates the subset constructions.
-		self.shifts = {} # Keys are symbols, values are state IDs.
-		self.reductions = {} # Keys refer to productions, values are about look-ahead, defined by the table generator.
-
-class HandleFindingAutomaton(typing.NamedTuple):
-	grammar: context_free.ContextFreeGrammar # Because what good is a parse table without most of the grammar also?
-	states: list #
-
-
-def glr0_construction(grammar:context_free.ContextFreeGrammar, start: (str, list, tuple)) -> HandleFindingAutomaton:
-	"""
+	This is not a method-of-CFG: that would have the wrong connotation. A grammar
+	specification is like a model, and this is more like a (complicated) view.
 	
-	:param grammar: as advertised
-	:param start: either a single start symbol or a group of acceptable ones.
-	:return: The
+	In principle you could drive a Tomita-style parser with this table and the
+	grammar definition, but that is not the plan.
 	"""
+	__slots__ = ['graph', 'initial', 'accept']
+	def __init__(self, grammar:context_free.ContextFreeGrammar):
+		"""
+		Basically, this is a breadth-first graph exploration/construction.
+		This variety of parse automaton does not consider look-ahead.
+		"""
+		
+		def optimize_unit_rules(step:dict, check:dict) -> dict:
+			# A unit-rule is eligible to be elided (optimized-to-nothing) exactly when
+			# the state reached by shifting the RHS would have no other active parse items.
+			# We don't actually traverse such states, but instead redirect the corresponding
+			# shift-entry, essentially running the unit-rule at table-generation time.
+			
+			# If performing this task while carrying look-ahead in the parse-items, it sufficient
+			# that the criteria would have applied had the look-ahead not been involved.
+			replace = {s: grammar.rules[r].lhs for s, r in check.items() if len(step[s]) == 1}
+			shifts = {}
+			for symbol in step.keys():
+				proxy = symbol
+				while proxy in replace: proxy = replace[proxy]
+				shifts[symbol] = bft.lookup(frozenset(step[proxy]), breadcrumb=proxy)
+			return shifts
+		
+		def build_state(core: frozenset):
+			step, check, state = collections.defaultdict(set), {}, GLR0_State(items=set(), shift={}, reduce=set())
+			def visit(item):
+				rule_id, position = item
+				if position < len(RHS[rule_id]):
+					next_symbol = RHS[rule_id][position]
+					step[next_symbol].add((rule_id,position+1)) # For the record,
+					if rule_id in unit_rules and position == 0: check[next_symbol] = rule_id
+					return symbol_front.get(next_symbol)
+				else: state.reduce.add(rule_id)
+			items = foundation.transitive_closure(core, visit)
+			self.graph.append(GLR0_State(
+				items=items,
+				shift=optimize_unit_rules(step, check),
+				reduce=set(r for r,p in items if p == len(RHS[r])) # The end-of-rule parse-items.
+			))
+		
+		assert grammar.start
+		##### Start by arranging most of the grammar data in a convenient form:
+		RHS = [rule.rhs for rule in grammar.rules] # This soon gets augmented internal to this algorithm.
+		unit_rules = set(i for i, rule in enumerate(grammar.rules) if rule.attribute is None and len(rule.rhs) == 1)
+		def front(rule_ids): return frozenset([(r,0) for r in rule_ids])
+		symbol_front = {symbol: front(rule_ids) for symbol, rule_ids in grammar.symbol_rule_ids.items()}
+		bft = foundation.BreadthFirstTraversal()
+		self.graph = []
+		# Initial-state cores refer only to the "augmentation" rule -- which has no LHS in this manifestation.
+		self.initial = [bft.lookup(front([foundation.allocate(RHS, [language])])) for language in grammar.start]
+		bft.execute(build_state)
+		self.accept = [self.graph[self.initial[language]].shift[language] for language in grammar.start]
+		
