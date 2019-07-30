f23cb11f7646365b2bd34b4d6ffd1a694925dd4b
Ian Kjos <kjosib@gmail.com>
2019-07-29 23:27:47 -0500

    Fix License

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ setuptools.setup(
 	name='booze-tools',
 	version='0.3.0',
 	packages=['boozetools', 'boozetools.macroparse'],
-	license='http://unlicense.org/',
 	description='A panoply of tools for parsing and lexical analysis',
 	long_description=open('README.md').read(),
 	long_description_content_type="text/markdown",
 	url="https://github.com/kjosib/booze-tools",
 	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: Public Domain",
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 		"Topic :: Software Development :: Compilers",
+		"Development Status :: 4 - Beta",
     ],
 )
